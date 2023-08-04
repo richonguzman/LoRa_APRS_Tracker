699b640fd1625af6b4c75d4b86c68235ace4b79a
richonguzman <richon2001@hotmail.com>
2023-08-04 00:13:51 -0400

    1.1

--- src/display.cpp
+++ src/display.cpp
@@ -1,9 +1,3 @@
-#ifdef TTGO_T_Beam_V1_0
-  #include <Adafruit_SSD1306.h>
-#endif
-#ifdef TTGO_T_Beam_V1_0_SH1106
-  #include <Adafruit_SH110X.h>
-#endif
 #include <Adafruit_GFX.h>
 #include <logger.h>
 #include <Wire.h>
@@ -11,6 +5,14 @@
 #include "configuration.h"
 #include "pins_config.h"
 #include "display.h"
+
+#define ssd1306 //uncomment this line when using SH1106 screen instead of SSD1306
+
+#ifdef ssd1306
+#include <Adafruit_SSD1306.h>
+#else
+#include <Adafruit_SH110X.h>
+#endif
 
 #define SYM_HEIGHT 14
 #define SYM_WIDTH  16
@@ -31,11 +33,10 @@ const uint8_t *symbolsAPRS[]  = {runnerSymbol, carSymbol, jeepSymbol, bikeSymbol
 
 extern logging::Logger logger;
 
-#ifdef TTGO_T_Beam_V1_0
-  Adafruit_SSD1306 display(128, 64, &Wire, OLED_RST);
-#endif
-#ifdef TTGO_T_Beam_V1_0_SH1106
-  Adafruit_SH1106G display(128, 64, &Wire, OLED_RST);
+#ifdef ssd1306
+Adafruit_SSD1306 display(128, 64, &Wire, OLED_RST);
+#else
+Adafruit_SH1106G display(128, 64, &Wire, OLED_RST);
 #endif
 
 // cppcheck-suppress unusedFunction
@@ -49,33 +50,30 @@ void setup_display() {
   #endif
 
   Wire.begin(OLED_SDA, OLED_SCL);
-  #ifdef TTGO_T_Beam_V1_0
-    if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3c, false, false)) {
-      logger.log(logging::LoggerLevel::LOGGER_LEVEL_ERROR, "SSD1306", "allocation failed!");
-      while (true) {
-      }
+  #ifdef ssd1306
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3c, false, false)) {
+    logger.log(logging::LoggerLevel::LOGGER_LEVEL_ERROR, "SSD1306", "allocation failed!");
+    while (true) {
     }
-  #endif
-  #ifdef TTGO_T_Beam_V1_0_SH1106
-    if (!display.begin(0x3c, true)) {
-      logger.log(logging::LoggerLevel::LOGGER_LEVEL_ERROR, "SH1106", "allocation failed!");
-      while (true) {
-      }
+  }
+  #else
+  if (!display.begin(0x3c, true)) {
+    logger.log(logging::LoggerLevel::LOGGER_LEVEL_ERROR, "SH1106", "allocation failed!");
+    while (true) {
     }
+  }
   #endif
-
   display.clearDisplay();
-  #ifdef TTGO_T_Beam_V1_0
-    display.setTextColor(WHITE);
+  #ifdef ssd1306
+  display.setTextColor(WHITE);
+  #else
+  display.setTextColor(SH110X_WHITE);
   #endif
-  #ifdef TTGO_T_Beam_V1_0_SH1106
-    display.setTextColor(SH110X_WHITE);
-  #endif  
   display.setTextSize(1);
   display.setCursor(0, 0);
-  #ifdef TTGO_T_Beam_V1_0
-    display.ssd1306_command(SSD1306_SETCONTRAST);
-    display.ssd1306_command(1);
+  #ifdef ssd1306
+  display.ssd1306_command(SSD1306_SETCONTRAST);
+  display.ssd1306_command(1);
   #endif
   display.display();
 }
@@ -83,12 +81,12 @@ void setup_display() {
 // cppcheck-suppress unusedFunction
 void display_toggle(bool toggle) {
   if (toggle) {
-    #ifdef TTGO_T_Beam_V1_0
-      display.ssd1306_command(SSD1306_DISPLAYON);
+    #ifdef ssd1306
+    display.ssd1306_command(SSD1306_DISPLAYON);
     #endif
   } else {
-    #ifdef TTGO_T_Beam_V1_0
-      display.ssd1306_command(SSD1306_DISPLAYOFF);
+    #ifdef ssd1306
+    display.ssd1306_command(SSD1306_DISPLAYOFF);
     #endif
   }
 }
@@ -96,18 +94,17 @@ void display_toggle(bool toggle) {
 // cppcheck-suppress unusedFunction
 void show_display(String header, int wait) {
   display.clearDisplay();
-  #ifdef TTGO_T_Beam_V1_0
-    display.setTextColor(WHITE);
-  #endif
-  #ifdef TTGO_T_Beam_V1_0_SH1106
-    display.setTextColor(SH110X_WHITE);
+  #ifdef ssd1306
+  display.setTextColor(WHITE);
+  #else
+  display.setTextColor(SH110X_WHITE);
   #endif  
   display.setTextSize(2);
   display.setCursor(0, 0);
   display.println(header);
-  #ifdef TTGO_T_Beam_V1_0
-    display.ssd1306_command(SSD1306_SETCONTRAST);
-    display.ssd1306_command(1);
+  #ifdef ssd1306
+  display.ssd1306_command(SSD1306_SETCONTRAST);
+  display.ssd1306_command(1);
   #endif
   display.display();
   delay(wait);
@@ -116,11 +113,10 @@ void show_display(String header, int wait) {
 // cppcheck-suppress unusedFunction
 void show_display(String header, String line1, int wait) {
   display.clearDisplay();
-  #ifdef TTGO_T_Beam_V1_0
-    display.setTextColor(WHITE);
-  #endif
-  #ifdef TTGO_T_Beam_V1_0_SH1106
-    display.setTextColor(SH110X_WHITE);
+  #ifdef ssd1306
+  display.setTextColor(WHITE);
+  #else
+  display.setTextColor(SH110X_WHITE);
   #endif  
   display.setTextSize(2);
   display.setCursor(0, 0);
@@ -128,9 +124,9 @@ void show_display(String header, String line1, int wait) {
   display.setTextSize(1);
   display.setCursor(0, 16);
   display.println(line1);
-  #ifdef TTGO_T_Beam_V1_0
-    display.ssd1306_command(SSD1306_SETCONTRAST);
-    display.ssd1306_command(1);
+  #ifdef ssd1306
+  display.ssd1306_command(SSD1306_SETCONTRAST);
+  display.ssd1306_command(1);
   #endif
   display.display();
   delay(wait);
@@ -139,11 +135,10 @@ void show_display(String header, String line1, int wait) {
 // cppcheck-suppress unusedFunction
 void show_display(String header, String line1, String line2, int wait) {
   display.clearDisplay();
-  #ifdef TTGO_T_Beam_V1_0
-    display.setTextColor(WHITE);
-  #endif
-  #ifdef TTGO_T_Beam_V1_0_SH1106
-    display.setTextColor(SH110X_WHITE);
+  #ifdef ssd1306
+  display.setTextColor(WHITE);
+  #else
+  display.setTextColor(SH110X_WHITE);
   #endif  
   display.setTextSize(2);
   display.setCursor(0, 0);
@@ -153,9 +148,9 @@ void show_display(String header, String line1, String line2, int wait) {
   display.println(line1);
   display.setCursor(0, 26);
   display.println(line2);
-  #ifdef TTGO_T_Beam_V1_0
-    display.ssd1306_command(SSD1306_SETCONTRAST);
-    display.ssd1306_command(1);
+  #ifdef ssd1306
+  display.ssd1306_command(SSD1306_SETCONTRAST);
+  display.ssd1306_command(1);
   #endif
   display.display();
   delay(wait);
@@ -164,11 +159,10 @@ void show_display(String header, String line1, String line2, int wait) {
 // cppcheck-suppress unusedFunction
 void show_display(String header, String line1, String line2, String line3, int wait) {
   display.clearDisplay();
-  #ifdef TTGO_T_Beam_V1_0
-    display.setTextColor(WHITE);
-  #endif
-  #ifdef TTGO_T_Beam_V1_0_SH1106
-    display.setTextColor(SH110X_WHITE);
+  #ifdef ssd1306
+  display.setTextColor(WHITE);
+  #else
+  display.setTextColor(SH110X_WHITE);
   #endif  
   display.setTextSize(2);
   display.setCursor(0, 0);
@@ -180,9 +174,9 @@ void show_display(String header, String line1, String line2, String line3, int w
   display.println(line2);
   display.setCursor(0, 36);
   display.println(line3);
-  #ifdef TTGO_T_Beam_V1_0
-    display.ssd1306_command(SSD1306_SETCONTRAST);
-    display.ssd1306_command(1);
+  #ifdef ssd1306
+  display.ssd1306_command(SSD1306_SETCONTRAST);
+  display.ssd1306_command(1);
   #endif
   display.display();
   delay(wait);
@@ -191,11 +185,10 @@ void show_display(String header, String line1, String line2, String line3, int w
 // cppcheck-suppress unusedFunction
 void show_display(String header, String line1, String line2, String line3, String line4, int wait) {
   display.clearDisplay();
-  #ifdef TTGO_T_Beam_V1_0
-    display.setTextColor(WHITE);
-  #endif
-  #ifdef TTGO_T_Beam_V1_0_SH1106
-    display.setTextColor(SH110X_WHITE);
+  #ifdef ssd1306
+  display.setTextColor(WHITE);
+  #else
+  display.setTextColor(SH110X_WHITE);
   #endif  
   display.setTextSize(2);
   display.setCursor(0, 0);
@@ -209,9 +202,9 @@ void show_display(String header, String line1, String line2, String line3, Strin
   display.println(line3);
   display.setCursor(0, 46);
   display.println(line4);
-  #ifdef TTGO_T_Beam_V1_0
-    display.ssd1306_command(SSD1306_SETCONTRAST);
-    display.ssd1306_command(1);
+  #ifdef ssd1306
+  display.ssd1306_command(SSD1306_SETCONTRAST);
+  display.ssd1306_command(1);
   #endif
   display.display();
   delay(wait);
@@ -220,11 +213,10 @@ void show_display(String header, String line1, String line2, String line3, Strin
 // cppcheck-suppress unusedFunction
 void show_display(String header, String line1, String line2, String line3, String line4, String line5, int wait) {
   display.clearDisplay();
-  #ifdef TTGO_T_Beam_V1_0
-    display.setTextColor(WHITE);
-  #endif
-  #ifdef TTGO_T_Beam_V1_0_SH1106
-    display.setTextColor(SH110X_WHITE);
+  #ifdef ssd1306
+  display.setTextColor(WHITE);
+  #else
+  display.setTextColor(SH110X_WHITE);
   #endif  
   display.setTextSize(2);
   display.setCursor(0, 0);
@@ -240,9 +232,9 @@ void show_display(String header, String line1, String line2, String line3, Strin
   display.println(line4);
   display.setCursor(0, 56);
   display.println(line5);
-  #ifdef TTGO_T_Beam_V1_0
-    display.ssd1306_command(SSD1306_SETCONTRAST);
-    display.ssd1306_command(1);
+  #ifdef ssd1306
+  display.ssd1306_command(SSD1306_SETCONTRAST);
+  display.ssd1306_command(1);
   #endif
 
   if (menuDisplay==0 && Config.showSymbolOnScreen) {
