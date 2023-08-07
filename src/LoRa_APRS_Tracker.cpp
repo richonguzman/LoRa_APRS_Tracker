2f44fb527d7f397470ba3d010da5d11647d4132c
Valentin Saugnier <valentin.s.10@gmail.com>
2023-08-07 13:14:22 +0200

    Fix eco mode

--- src/LoRa_APRS_Tracker.cpp
+++ src/LoRa_APRS_Tracker.cpp
@@ -42,6 +42,7 @@ std::vector<String>           loadedAPRSMessages;
 bool      displayEcoMode      = Config.displayEcoMode;
 bool      displayState        = true;
 uint32_t  displayTime         = millis();
+uint32_t  refreshDisplayTime   = millis();
 
 bool      sendUpdate          = true;
 int       updateCounter       = Config.sendCommentAfterXBeacons;
@@ -137,9 +138,9 @@ void loop() {
     STATION_Utils::checkSmartBeaconInterval(currentSpeed);
   }
 
-  if (millis() - displayTime >= 1000 || gps_time_update) {
+  if (millis() - refreshDisplayTime >= 1000 || gps_time_update) {
     GPS_Utils::checkStartUpFrames();
     MENU_Utils::showOnScreen();
-    displayTime = millis();
+    refreshDisplayTime = millis();
   }
 }
