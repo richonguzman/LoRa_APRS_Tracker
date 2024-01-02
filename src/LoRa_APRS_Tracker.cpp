4b269dc3523f702097747e95e4dcbd067721cfcd
richonguzman <richon2001@hotmail.com>
2024-01-02 00:33:03 -0300

    few updates for Tbeam Supreme

--- src/LoRa_APRS_Tracker.cpp
+++ src/LoRa_APRS_Tracker.cpp
@@ -34,7 +34,7 @@ BluetoothSerial               SerialBT;
 OneButton userButton          = OneButton(BUTTON_PIN, true, true);
 #endif
 
-String    versionDate         = "2024.01.01";
+String    versionDate         = "2024.01.02";
 
 int       myBeaconsIndex      = 0;
 int       myBeaconsSize       = Config.beacons.size();
