52107931692f47f19aabfc3d74b330f988cf2430
richonguzman <richon2001@hotmail.com>
2024-01-05 18:59:36 -0300

    version update

--- src/LoRa_APRS_Tracker.cpp
+++ src/LoRa_APRS_Tracker.cpp
@@ -34,7 +34,7 @@ BluetoothSerial               SerialBT;
 OneButton userButton          = OneButton(BUTTON_PIN, true, true);
 #endif
 
-String    versionDate         = "2024.01.04";
+String    versionDate         = "2024.01.05";
 
 int       myBeaconsIndex      = 0;
 int       myBeaconsSize       = Config.beacons.size();
