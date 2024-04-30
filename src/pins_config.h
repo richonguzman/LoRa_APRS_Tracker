4f7bbc4afddf8f1d0ac762ee14681b60f6c9fce2
richonguzman <richon2001@hotmail.com>
2024-04-30 17:10:03 -0400

    pins config comments

--- src/pins_config.h
+++ src/pins_config.h
@@ -130,7 +130,7 @@
 #define BUTTON_PIN          0
 #define BATTERY_PIN         1
 #define VExt_CTRL           36
-#define ADC_CTRL            37
+#define ADC_CTRL            37  // Heltec V3 needs ADC_CTRL = LOW powers the voltage divider to read BatteryPin
 #define RADIO_SCLK_PIN      9
 #define RADIO_MISO_PIN      11
 #define RADIO_MOSI_PIN      10
@@ -147,8 +147,8 @@
 #define GPS_RESET           35
 #define BUTTON_PIN          0
 #define BATTERY_PIN         1
-#define ADC_CTRL            2   // active HIGH, powers the voltage divider. Only on V05 = V1.1
-#define VExt_CTRL           3   // Heltec Tracker needs this (pulled low?) for GPS and TFT
+#define ADC_CTRL            2   // HELTEC Wireless Tracker ADC_CTRL = HIGH powers the voltage divider to read BatteryPin. Only on V05 = V1.1
+#define VExt_CTRL           3   // To turn on GPS and TFT
 #define BOARD_I2C_SDA       7
 #define BOARD_I2C_SCL       6
 #define RADIO_SCLK_PIN      9
