1247c9a202296fd19516f8ca0e73a55ef25e33d0
richonguzman <richon2001@hotmail.com>
2024-05-16 11:53:00 -0400

    radiolib tbeam sx126-2-8

--- src/boards_pinout.h
+++ src/boards_pinout.h
@@ -6,7 +6,7 @@
 #undef OLED_RST
 
 
-#if defined(TTGO_T_Beam_V1_0) || defined(TTGO_T_Beam_V1_2) || defined(TTGO_T_Beam_V1_0_SX1268) || defined(TTGO_T_Beam_V1_2_SX1262)
+#if defined(TTGO_T_Beam_V1_0) || defined(TTGO_T_Beam_V1_2)
     #define OLED_SDA            21
     #define OLED_SCL            22
     #define OLED_RST            16
@@ -19,6 +19,41 @@
     #define RADIO_CS_PIN        18  // CS  --> NSS
     #define RADIO_RST_PIN       23
     #define RADIO_BUSY_PIN      26  // IRQ --> DIO0
+#endif
+
+
+#if defined(TTGO_T_Beam_V1_0_SX1268) || defined(TTGO_T_Beam_V1_2_SX1262)
+    #define OLED_SDA            21
+    #define OLED_SCL            22
+    #define OLED_RST            16
+    #define GPS_RX              12
+    #define GPS_TX              34
+    #define BUTTON_PIN          38
+    #define RADIO_SCLK_PIN      5
+    #define RADIO_MISO_PIN      19
+    #define RADIO_MOSI_PIN      27
+    #define RADIO_CS_PIN        18
+    #define RADIO_DIO0_PIN      26
+    #define RADIO_RST_PIN       23
+    #define RADIO_DIO1_PIN      33
+    #define RADIO_BUSY_PIN      32
+#endif
+
+
+#ifdef TTGO_T_Beam_V0_7
+    #define OLED_SDA            21
+    #define OLED_SCL            22
+    #define OLED_RST            16
+    #define GPS_RX              15
+    #define GPS_TX              12
+    #define BUTTON_PIN          39
+    #define BATTERY_PIN         35
+    #define RADIO_SCLK_PIN      5
+    #define RADIO_MISO_PIN      19
+    #define RADIO_MOSI_PIN      27
+    #define RADIO_CS_PIN        18
+    #define RADIO_RST_PIN       23
+    #define RADIO_BUSY_PIN      26
 #endif
 
 
@@ -60,35 +95,6 @@
     #define RADIO_BUSY_PIN      14
     #define RADIO_RXEN          32
     #define RADIO_TXEN          25
-#endif
-
-
-#if defined(TTGO_T_Beam_V1_0_SX1268) || defined(TTGO_T_Beam_V1_2_SX1262)
-    #define RADIO_SCLK_PIN      5
-    #define RADIO_MISO_PIN      19
-    #define RADIO_MOSI_PIN      27
-    #define RADIO_CS_PIN        18
-    #define RADIO_DIO0_PIN      26
-    #define RADIO_RST_PIN       23
-    #define RADIO_DIO1_PIN      33
-    #define RADIO_BUSY_PIN      32
-#endif
-
-
-#ifdef TTGO_T_Beam_V0_7
-    #define OLED_SDA            21
-    #define OLED_SCL            22
-    #define OLED_RST            16
-    #define GPS_RX              15
-    #define GPS_TX              12
-    #define BUTTON_PIN          39
-    #define BATTERY_PIN         35
-    #define RADIO_SCLK_PIN      5
-    #define RADIO_MISO_PIN      19
-    #define RADIO_MOSI_PIN      27
-    #define RADIO_CS_PIN        18
-    #define RADIO_RST_PIN       23
-    #define RADIO_BUSY_PIN      26
 #endif
 
 
