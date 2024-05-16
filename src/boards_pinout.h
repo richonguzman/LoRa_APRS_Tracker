06a60ad9ccef9406cc764ad43eec4f3758eb31c2
richonguzman <richon2001@hotmail.com>
2024-05-16 12:35:21 -0400

    tbeam supreme cleanup

--- src/boards_pinout.h
+++ src/boards_pinout.h
@@ -121,6 +121,24 @@
 #endif
 
 
+#ifdef TTGO_T_Beam_S3_SUPREME_V3
+    #define OLED_SDA            17
+    #define OLED_SCL            18
+    #define OLED_RST            16
+    #define GPS_RX              8
+    #define GPS_TX              9
+    #define BUTTON_PIN          0
+    #define RADIO_SCLK_PIN      12
+    #define RADIO_MISO_PIN      13
+    #define RADIO_MOSI_PIN      11
+    #define RADIO_CS_PIN        10
+    #define RADIO_DIO0_PIN      -1
+    #define RADIO_RST_PIN       5
+    #define RADIO_DIO1_PIN      1
+    #define RADIO_BUSY_PIN      4
+#endif
+
+
 #ifdef ESP32_DIY_1W_LoRa_GPS
     #define OLED_SDA            21
     #define OLED_SCL            22
@@ -141,22 +159,7 @@
 #endif
 
 
-#ifdef TTGO_T_Beam_S3_SUPREME_V3
-    #define OLED_SDA            17
-    #define OLED_SCL            18
-    #define OLED_RST            16
-    #define GPS_RX              8
-    #define GPS_TX              9
-    #define BUTTON_PIN          0
-    #define RADIO_SCLK_PIN      12
-    #define RADIO_MISO_PIN      13
-    #define RADIO_MOSI_PIN      11
-    #define RADIO_CS_PIN        10
-    #define RADIO_DIO0_PIN      -1
-    #define RADIO_RST_PIN       5
-    #define RADIO_DIO1_PIN      1
-    #define RADIO_BUSY_PIN      4
-#endif
+
 
 
 #ifdef OE5HWN_MeshCom
