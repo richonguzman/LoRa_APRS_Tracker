32c99218d68e8c095cc0f0a55ae38c010b3d0f79
richonguzman <richon2001@hotmail.com>
2024-06-05 23:49:00 -0400

    more const cleaning

--- src/bme_utils.h
+++ src/bme_utils.h
@@ -10,8 +10,8 @@
  
 namespace BME_Utils {
 
-    void    getWxModuleAddres();
-    void    setup();
+    void  getWxModuleAddres();
+    void  setup();
     const String generateTempString(const float bmeTemp, const uint8_t type);
     const String generateHumString(const float bmeHum, const uint8_t type);
     const String generatePresString(const float bmePress, const uint8_t type);
