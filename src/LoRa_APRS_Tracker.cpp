d7638fa09488d7b1ce9968c01bd5dc544099c94b
richonguzman <richon2001@hotmail.com>
2024-08-12 09:21:25 -0400

    version Update

--- src/LoRa_APRS_Tracker.cpp
+++ src/LoRa_APRS_Tracker.cpp
@@ -46,7 +46,7 @@ TinyGPSPlus                         gps;
     OneButton userButton            = OneButton(BUTTON_PIN, true, true);
 #endif
 
-String      versionDate             = "2024.08.02";
+String      versionDate             = "2024.08.12";
 
 uint8_t     myBeaconsIndex          = 0;
 int         myBeaconsSize           = Config.beacons.size();
