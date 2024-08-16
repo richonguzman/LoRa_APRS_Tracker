75ca1d72ab6c0c2a9af566094af5e5e6aaece336
richonguzman <richon2001@hotmail.com>
2024-08-16 09:06:58 -0400

    updated conf code

--- src/configuration.cpp
+++ src/configuration.cpp
@@ -27,83 +27,83 @@ void Configuration::readFile(fs::FS &fs, const char *fileName) {
     for (int i = 0; i < BeaconsArray.size(); i++) {
         Beacon bcn;
 
-        bcn.callsign            = BeaconsArray[i]["callsign"].as<String>();
+        bcn.callsign            = BeaconsArray[i]["callsign"] | "NOCALL-7";
         bcn.callsign.toUpperCase();
-        bcn.gpsEcoMode          = BeaconsArray[i]["gpsEcoMode"].as<bool>();
-        bcn.symbol              = BeaconsArray[i]["symbol"].as<String>();
-        bcn.overlay             = BeaconsArray[i]["overlay"].as<String>();
-        bcn.micE                = BeaconsArray[i]["micE"].as<String>();
-        bcn.comment             = BeaconsArray[i]["comment"].as<String>();
-        bcn.smartBeaconState    = BeaconsArray[i]["smartBeacon"]["active"].as<bool>();
-        bcn.slowRate            = BeaconsArray[i]["smartBeacon"]["slowRate"].as<int>();
-        bcn.slowSpeed           = BeaconsArray[i]["smartBeacon"]["slowSpeed"].as<int>();
-        bcn.fastRate            = BeaconsArray[i]["smartBeacon"]["fastRate"].as<int>();
-        bcn.fastSpeed           = BeaconsArray[i]["smartBeacon"]["fastSpeed"].as<int>();
-        bcn.minTxDist           = BeaconsArray[i]["smartBeacon"]["minTxDist"].as<int>();
-        bcn.minDeltaBeacon      = BeaconsArray[i]["smartBeacon"]["minDeltaBeacon"].as<int>();
-        bcn.turnMinDeg          = BeaconsArray[i]["smartBeacon"]["turnMinDeg"].as<int>();
-        bcn.turnSlope           = BeaconsArray[i]["smartBeacon"]["turnSlope"].as<int>();
+        bcn.gpsEcoMode          = BeaconsArray[i]["gpsEcoMode"] | false;
+        bcn.symbol              = BeaconsArray[i]["symbol"] | ">";
+        bcn.overlay             = BeaconsArray[i]["overlay"] | "/";
+        bcn.micE                = BeaconsArray[i]["micE"] | "";
+        bcn.comment             = BeaconsArray[i]["comment"] | "";
+        bcn.smartBeaconState    = BeaconsArray[i]["smartBeacon"]["active"] | true;
+        bcn.slowRate            = BeaconsArray[i]["smartBeacon"]["slowRate"] | 120;
+        bcn.slowSpeed           = BeaconsArray[i]["smartBeacon"]["slowSpeed"] | 10;
+        bcn.fastRate            = BeaconsArray[i]["smartBeacon"]["fastRate"] | 60;
+        bcn.fastSpeed           = BeaconsArray[i]["smartBeacon"]["fastSpeed"]| 70;
+        bcn.minTxDist           = BeaconsArray[i]["smartBeacon"]["minTxDist"] | 100;
+        bcn.minDeltaBeacon      = BeaconsArray[i]["smartBeacon"]["minDeltaBeacon"] | 12;
+        bcn.turnMinDeg          = BeaconsArray[i]["smartBeacon"]["turnMinDeg"] | 10;
+        bcn.turnSlope           = BeaconsArray[i]["smartBeacon"]["turnSlope"] | 80;
         
         beacons.push_back(bcn);
     }
 
-    display.showSymbol            = data["display"]["showSymbol"].as<bool>();
-    display.ecoMode               = data["display"]["ecoMode"].as<bool>();
-    display.timeout               = data["display"]["timeout"].as<int>();
-    display.turn180               = data["display"]["turn180"].as<bool>();
+    display.showSymbol            = data["display"]["showSymbol"] | true;
+    display.ecoMode               = data["display"]["ecoMode"] | false;
+    display.timeout               = data["display"]["timeout"] | 4;
+    display.turn180               = data["display"]["turn180"] | false;
 
-    winlink.password              = data["winlink"]["password"].as<String>();
+    winlink.password              = data["winlink"]["password"] | "NOPASS";
 
-    bme.active                    = data["bme"]["active"].as<bool>();
-    bme.temperatureCorrection     = data["bme"]["temperatureCorrection"].as<float>();
-    bme.sendTelemetry             = data["bme"]["sendTelemetry"].as<bool>();
+    bme.active                    = data["bme"]["active"] | false;
+    bme.temperatureCorrection     = data["bme"]["temperatureCorrection"] | 0.0;
+    bme.sendTelemetry             = data["bme"]["sendTelemetry"] | false;
 
-    notification.ledTx            = data["notification"]["ledTx"].as<bool>();
-    notification.ledTxPin         = data["notification"]["ledTxPin"].as<int>();
-    notification.ledMessage       = data["notification"]["ledMessage"].as<bool>();
-    notification.ledMessagePin    = data["notification"]["ledMessagePin"].as<int>();
-    notification.ledFlashlight    = data["notification"]["ledFlashlight"].as<bool>();
-    notification.ledFlashlightPin = data["notification"]["ledFlashlightPin"].as<int>();
-    notification.buzzerActive     = data["notification"]["buzzerActive"].as<bool>();
-    notification.buzzerPinTone    = data["notification"]["buzzerPinTone"].as<int>();
-    notification.buzzerPinVcc     = data["notification"]["buzzerPinVcc"].as<int>();
-    notification.bootUpBeep       = data["notification"]["bootUpBeep"].as<bool>();
-    notification.txBeep           = data["notification"]["txBeep"].as<bool>();
-    notification.messageRxBeep    = data["notification"]["messageRxBeep"].as<bool>();
-    notification.stationBeep      = data["notification"]["stationBeep"].as<bool>();
-    notification.lowBatteryBeep   = data["notification"]["lowBatteryBeep"].as<bool>();
-    notification.shutDownBeep     = data["notification"]["shutDownBeep"].as<bool>();
+    notification.ledTx            = data["notification"]["ledTx"] | false;
+    notification.ledTxPin         = data["notification"]["ledTxPin"]| 13;
+    notification.ledMessage       = data["notification"]["ledMessage"] | false;
+    notification.ledMessagePin    = data["notification"]["ledMessagePin"] | 2;
+    notification.ledFlashlight    = data["notification"]["ledFlashlight"] | false;
+    notification.ledFlashlightPin = data["notification"]["ledFlashlightPin"] | 14;
+    notification.buzzerActive     = data["notification"]["buzzerActive"] | false;
+    notification.buzzerPinTone    = data["notification"]["buzzerPinTone"] | 33;
+    notification.buzzerPinVcc     = data["notification"]["buzzerPinVcc"] | 25;
+    notification.bootUpBeep       = data["notification"]["bootUpBeep"] | false;
+    notification.txBeep           = data["notification"]["txBeep"] | false;
+    notification.messageRxBeep    = data["notification"]["messageRxBeep"] | false;
+    notification.stationBeep      = data["notification"]["stationBeep"] | false;
+    notification.lowBatteryBeep   = data["notification"]["lowBatteryBeep"] | false;
+    notification.shutDownBeep     = data["notification"]["shutDownBeep"] | false;
 
     JsonArray LoraTypesArray = data["lora"];
     for (int j = 0; j < LoraTypesArray.size(); j++) {
         LoraType loraType;
 
-        loraType.frequency           = LoraTypesArray[j]["frequency"].as<long>();
-        loraType.spreadingFactor     = LoraTypesArray[j]["spreadingFactor"].as<int>();
-        loraType.signalBandwidth     = LoraTypesArray[j]["signalBandwidth"].as<long>();
-        loraType.codingRate4         = LoraTypesArray[j]["codingRate4"].as<int>();
-        loraType.power               = LoraTypesArray[j]["power"].as<int>();
+        loraType.frequency           = LoraTypesArray[j]["frequency"] | 433775000;
+        loraType.spreadingFactor     = LoraTypesArray[j]["spreadingFactor"] | 12;
+        loraType.signalBandwidth     = LoraTypesArray[j]["signalBandwidth"] | 125000;
+        loraType.codingRate4         = LoraTypesArray[j]["codingRate4"] | 5;
+        loraType.power               = LoraTypesArray[j]["power"] | 20;
         loraTypes.push_back(loraType);
     }
 
-    ptt.active                    = data["pttTrigger"]["active"].as<bool>();
-    ptt.io_pin                    = data["pttTrigger"]["io_pin"].as<int>();
-    ptt.preDelay                  = data["pttTrigger"]["preDelay"].as<int>();
-    ptt.postDelay                 = data["pttTrigger"]["postDelay"].as<int>();
-    ptt.reverse                   = data["pttTrigger"]["reverse"].as<bool>();
+    ptt.active                    = data["pttTrigger"]["active"] | false;
+    ptt.io_pin                    = data["pttTrigger"]["io_pin"] | 4;
+    ptt.preDelay                  = data["pttTrigger"]["preDelay"] | 0;
+    ptt.postDelay                 = data["pttTrigger"]["postDelay"] | 0;
+    ptt.reverse                   = data["pttTrigger"]["reverse"] | false;
 
-    simplifiedTrackerMode         = data["other"]["simplifiedTrackerMode"].as<bool>();
-    sendCommentAfterXBeacons      = data["other"]["sendCommentAfterXBeacons"].as<int>();
-    path                          = data["other"]["path"].as<String>();
-    nonSmartBeaconRate            = data["other"]["nonSmartBeaconRate"].as<int>();
-    rememberStationTime           = data["other"]["rememberStationTime"].as<int>();
-    maxDistanceToTracker          = data["other"]["maxDistanceToTracker"].as<int>();
-    standingUpdateTime            = data["other"]["standingUpdateTime"].as<int>();
-    sendAltitude                  = data["other"]["sendAltitude"].as<bool>();
-    sendBatteryInfo               = data["other"]["sendBatteryInfo"].as<bool>();
-    bluetoothType                 = data["other"]["bluetoothType"].as<int>();
-    bluetoothActive               = data["other"]["bluetoothActive"].as<bool>();
-    disableGPS                    = data["other"]["disableGPS"].as<bool>();
+    simplifiedTrackerMode         = data["other"]["simplifiedTrackerMode"] | false;
+    sendCommentAfterXBeacons      = data["other"]["sendCommentAfterXBeacons"] | 10;
+    path                          = data["other"]["path"] | "WIDE1-1";
+    nonSmartBeaconRate            = data["other"]["nonSmartBeaconRate"] | 15;
+    rememberStationTime           = data["other"]["rememberStationTime"] | 30;
+    maxDistanceToTracker          = data["other"]["maxDistanceToTracker"] | 30;
+    standingUpdateTime            = data["other"]["standingUpdateTime"] | 15;
+    sendAltitude                  = data["other"]["sendAltitude"] | true;
+    sendBatteryInfo               = data["other"]["sendBatteryInfo"] | false;
+    bluetoothType                 = data["other"]["bluetoothType"] | 1;
+    bluetoothActive               = data["other"]["bluetoothActive"] | true;
+    disableGPS                    = data["other"]["disableGPS"] | false;
 
     configFile.close();
 }
