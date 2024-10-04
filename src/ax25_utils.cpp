b13888f42aed4ca92fedfdf228bf74ceb8c869e8
Damian <155308374+SQ2CPA@users.noreply.github.com>
2024-10-04 18:51:13 +0200

    improvement: update KISS AX.25 functions from IGate software

--- src/ax25_utils.cpp
+++ src/ax25_utils.cpp
@@ -1,165 +1,172 @@
 #include "ax25_utils.h"
+#include "kiss_protocol.h"
 
 namespace AX25_Utils {
 
-    AX25Frame decodedFrame;
+    bool validateTNC2Frame(const String& tnc2FormattedFrame) {
+        return (tnc2FormattedFrame.indexOf(':') != -1) && (tnc2FormattedFrame.indexOf('>') != -1);
+    }
 
-    String decodeFrame(const String& frame) {
-        String packet = "";
-        for (int a = 0; a < 6; a ++) {
-            uint16_t shiftedValue = frame[a] >> 1;
-            if (shiftedValue == 32) {
-                a = 10;
+    bool validateKISSFrame(const String& kissFormattedFrame) {
+        return kissFormattedFrame.charAt(0) == (char)FEND && kissFormattedFrame.charAt(kissFormattedFrame.length() - 1) == (char)FEND;
+    }
+
+    String encodeAddressAX25(String tnc2Address) {
+        bool hasBeenDigipited = tnc2Address.indexOf('*') != -1;
+
+        if (tnc2Address.indexOf('-') == -1) {
+            if (hasBeenDigipited) {
+                tnc2Address = tnc2Address.substring(0, tnc2Address.length() - 1);
+            }
+
+            tnc2Address += "-0";
+        }
+
+        int separatorIndex = tnc2Address.indexOf('-');
+        int ssid = tnc2Address.substring(separatorIndex + 1).toInt();
+
+        String kissAddress = "";
+        for (int i = 0; i < 6; ++i) {
+            char addressChar;
+            if (tnc2Address.length() > i && i < separatorIndex) {
+                addressChar = tnc2Address.charAt(i);
             } else {
-                //Serial.print(char(shiftedValue));
-                packet += char(shiftedValue);
+                addressChar = ' ';
             }
+            kissAddress += (char)(addressChar << 1);
         }
-        byte ssid = (frame[6]>>1) & 0x0f;
-        if (String(ssid) != "0") {
-            packet += "-";
-            packet += String(ssid);
-        }
-        return packet;
+
+        kissAddress += (char)((ssid << 1) | 0b01100000 | (hasBeenDigipited ? HAS_BEEN_DIGIPITED_MASK : 0));
+        return kissAddress;
     }
 
-    bool decodeAX25(const String& frame, int frameSize, AX25Frame* decodedFrame) {
-        if ((frameSize < 14) || (frame[0] != KissChar::Fend && frame[1] != KissCmd::Data && frame[frameSize - 1] != KissChar::Fend)) {
-            return false;
-        }
-        int payloadFrameStart = 0;
-        for (int i = 0; i < frameSize; i++) {                   // where is CONTROL y PID ?
-            if (frame[i] == 0x03 && frame[i+1] == 0xf0) {
-                payloadFrameStart = i+1;
+    String decodeAddressAX25(const String& ax25Address, bool& isLast, bool isRelay) {
+        String address = "";
+        for (int i = 0; i < 6; ++i) {
+            uint8_t currentCharacter = ax25Address.charAt(i);
+            currentCharacter >>= 1;
+            if (currentCharacter != ' ') {
+                address += (char)currentCharacter;
             }
         }
-        decodedFrame->tocall    = frame.substring(2, 9);      // Extract destination address
-        decodedFrame->sender    = frame.substring(9, 16);     // Extract source address
-        if (payloadFrameStart >= 21) {                    // is there path1?
-            decodedFrame->path1 = frame.substring(16, 23);
+        auto ssidChar = (uint8_t)ax25Address.charAt(6);
+        bool hasBeenDigipited = ssidChar & HAS_BEEN_DIGIPITED_MASK;
+        isLast = ssidChar & IS_LAST_ADDRESS_POSITION_MASK;
+        ssidChar >>= 1;
+
+        int ssid = 0b1111 & ssidChar;
+
+        if (ssid) {
+            address += '-';
+            address += ssid;
         }
-        if (payloadFrameStart >= 28) {                    // is there path2?
-            decodedFrame->path2 = frame.substring(23, 30);
+        if (isRelay && hasBeenDigipited) {
+            address += '*';
         }
-        decodedFrame->control   = frame.substring(payloadFrameStart-1, payloadFrameStart);   // Extract control information  // 0x03
-        decodedFrame->pid       = frame.substring(payloadFrameStart, payloadFrameStart + 1);       // Extract pid information      // 0xF0
-        decodedFrame->payload   = frame.substring(payloadFrameStart + 1, frameSize - 1);         // Extract payload
-        return true;
+
+        return address;
     }
 
-    String AX25FrameToLoRaPacket(const String& frame) {
-        //Serial.println(frame);
-        if (decodeAX25(frame, frame.length(), &decodedFrame)) {
-            //String packetToLoRa = "";
-            String packetToLoRa = decodeFrame(decodedFrame.sender) + ">" + decodeFrame(decodedFrame.tocall);
+    String encapsulateKISS(const String& ax25Frame, uint8_t cmd) {
+        String kissFrame = "";
+        kissFrame += (char)FEND;
+        kissFrame += (char)(0x0f & cmd);
 
-            if (decodedFrame.path1[0] != 0) {
-                packetToLoRa += ",";
-                packetToLoRa += decodeFrame(decodedFrame.path1);
+        for (int i = 0; i < ax25Frame.length(); ++i) {
+            char currentChar = ax25Frame.charAt(i);
+            if (currentChar == (char)FEND) {
+                kissFrame += (char)FESC;
+                kissFrame += (char)TFEND;
+            } else if (currentChar == (char)FESC) {
+                kissFrame += (char)FESC;
+                kissFrame += (char)TFESC;
+            } else {
+                kissFrame += currentChar;
             }
-            if (decodedFrame.path2[0] != 0) {
-                packetToLoRa += ",";
-                packetToLoRa += decodeFrame(decodedFrame.path2);
-            }
-            packetToLoRa += ":";
-            packetToLoRa += decodedFrame.payload;
-            return packetToLoRa;
-        } else {
-            return "";
         }
+        kissFrame += (char)FEND; // end of frame
+        return kissFrame;
     }
 
-    String frameCleaning(const String& frameToClean) {
-        String frame = frameToClean;
-        if (frame.length() > 6) {
-            frame = frame.substring(0, 6);
-        } else if (frame.length() < 6) {
-            for (int i = 0; frame.length() < 6; i++) { 
-                frame += " ";
+    String decapsulateKISS(const String& frame) {
+        String ax25Frame = "";
+        for (int i = 2; i < frame.length() - 1; ++i) {
+            char currentChar = frame.charAt(i);
+            if (currentChar == (char)FESC) {
+                char nextChar = frame.charAt(i + 1);
+                if (nextChar == (char)TFEND) {
+                    ax25Frame += (char)FEND;
+                } else if (nextChar == (char)TFESC) {
+                    ax25Frame += (char)FESC;
+                }
+                i++;
+            } else {
+                ax25Frame += currentChar;
             }
         }
+
+        return ax25Frame;
+    }
+
+    String encodeKISS(const String& frame) {
+        String ax25Frame = "";
+
+        if (validateTNC2Frame(frame)) {
+            String address = "";
+            bool dstAddresWritten = false;
+            for (int p = 0; p <= frame.indexOf(':'); p++) {
+                char currentChar = frame.charAt(p);
+                if (currentChar == ':' || currentChar == '>' || currentChar == ',') {
+                    if (!dstAddresWritten && (currentChar == ',' || currentChar == ':')) {
+                        ax25Frame = encodeAddressAX25(address) + ax25Frame;
+                        dstAddresWritten = true;
+                    } else {
+                        ax25Frame += encodeAddressAX25(address);
+                    }
+                    address = "";
+                } else {
+                    address += currentChar;
+                }
+            }
+
+            auto lastAddressChar = (uint8_t)ax25Frame.charAt(ax25Frame.length() - 1);
+            ax25Frame.setCharAt(ax25Frame.length() - 1, (char)(lastAddressChar | IS_LAST_ADDRESS_POSITION_MASK));
+            ax25Frame += (char)APRS_CONTROL_FIELD;
+            ax25Frame += (char)APRS_INFORMATION_FIELD;
+            ax25Frame += frame.substring(frame.indexOf(':') + 1);
+        }
+
+        String kissFrame = encapsulateKISS(ax25Frame, CMD_DATA);
+        return kissFrame;
+    }
+
+    String decodeKISS(const String& inputFrame, bool& dataFrame) {
+        String frame = "";
+
+        if (validateKISSFrame(inputFrame)) {
+            dataFrame = inputFrame.charAt(1) == CMD_DATA;
+            if (dataFrame) {
+                String ax25Frame = decapsulateKISS(inputFrame);
+                bool isLast = false;
+                String dstAddr = decodeAddressAX25(ax25Frame.substring(0, 7), isLast, false);
+                String srcAddr = decodeAddressAX25(ax25Frame.substring(7, 14), isLast, false);
+
+                frame = srcAddr + ">" + dstAddr;
+
+                int digiInfoIndex = 14;
+                while (!isLast && digiInfoIndex + 7 < ax25Frame.length()) {
+                    String digiAddr = decodeAddressAX25(ax25Frame.substring(digiInfoIndex, digiInfoIndex + 7), isLast, true);
+                    frame += ',' + digiAddr;
+                    digiInfoIndex += 7;
+                }
+
+                frame += ':';
+                frame += ax25Frame.substring(digiInfoIndex + 2);
+            } else {
+                frame += inputFrame;
+            }
+        }
+
         return frame;
     }
-
-    std::string intToBinaryString(uint8_t value, const uint8_t bitLength) {
-        std::string result = "";
-        //result.reserve(bitLength);
-        for (int i = bitLength - 1; i >= 0; i--) {
-            result += ((value >> i) & 1) ? '1' : '0';
-        }
-        return result;
-    }
-
-    String encodeAX25Address(const String& frame, uint8_t type, const bool lastAddress) {
-        String address;
-        int ssid;
-        if (frame.indexOf("-") > 0) {
-            address = frameCleaning(frame.substring(0, frame.indexOf("-")));
-            ssid = frame.substring(frame.indexOf("-") + 1).toInt();
-            if (ssid > 15) {
-                ssid = 0;
-            }
-        } else {
-            address = frameCleaning(frame);
-            ssid = 0;
-        }
-        String packet = "";
-        for (int j = 0; j < 6; j++) {
-            char c = address[j];
-            packet += char(c<<1);
-        }
-        std::string firstSSIDBit = std::to_string(type); //type=0 (sender or path not repeated) type=1 (tocall or path being repeated)
-        std::string lastSSIDBit = "0";
-        if (lastAddress) {
-            lastSSIDBit = "1";          // address is the last from AX.25 Frame
-        }
-        std::string concatenatedBinary = firstSSIDBit + "11" + intToBinaryString(ssid,4) + lastSSIDBit; // ( CRRSSSSX / HRRSSSSX )
-        long decimalValue = strtol(concatenatedBinary.c_str(), NULL, 2);
-        packet += (char)decimalValue;   //SSID 
-        return packet;
-    }
-
-    String LoRaPacketToAX25Frame(const String& packet) {
-        //String encodedPacket    = "";
-        //String tocall           = "";
-        String  sender      = packet.substring(0, packet.indexOf(">"));
-        bool    lastAddress = false;
-        //String  payload     = packet.substring(packet.indexOf(":") + 1);
-        String  temp        = packet.substring(packet.indexOf(">") + 1, packet.indexOf(":"));
-
-        String tocall;
-        if (temp.indexOf(",") != -1) {    
-            tocall      = temp.substring(0, temp.indexOf(","));
-            temp        = temp.substring(temp.indexOf(",") + 1);
-        } else {
-            tocall      = temp;
-            temp        = "";
-            lastAddress = true;
-        }
-        String encodedPacket = encodeAX25Address(tocall, 1, false);
-        encodedPacket += encodeAX25Address(sender, 0, lastAddress);
-
-        while (temp.length() > 0) {
-            String address;
-            if (temp.indexOf(",") > 0) {
-                address     = temp.substring(0, temp.indexOf(","));
-                temp        = temp.substring(temp.indexOf(",") + 1);
-            } else {
-                address     = temp;
-                temp        = "";
-                lastAddress = true;        
-            }
-            int repeatedPath = 0;
-            if (address.indexOf("*") > 0) {
-                repeatedPath = 1;
-            }
-            encodedPacket += encodeAX25Address(address, repeatedPath, lastAddress);
-        }
-
-        encodedPacket += char(0x03);
-        encodedPacket += char(0xF0);
-        encodedPacket += packet.substring(packet.indexOf(":") + 1);
-        return encodedPacket;
-    }
-
 }
